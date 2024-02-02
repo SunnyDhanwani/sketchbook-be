1b044afad8aebce09251ccc8f1cb90051e430d8a
Sunny Dhanwani <dhanwanisunny678@gmail.com>
2024-02-02 21:15:32 +0530

    Added FE url to env

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: process.env.FE_URL }));
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   /* options */
-  cors: "http://localhost:3000",
+  cors: process.env.FE_URL,
 });
 
 io.on("connection", (socket) => {
-  // ...
-  console.log("Server connected");
+  socket.on("beginPath", (arg) => {
+    socket.broadcast.emit("beginPath", arg);
+  });
+
+  socket.on("drawLine", (arg) => {
+    socket.broadcast.emit("drawLine", arg);
+  });
+
+  socket.on("changeConfig", (arg) => {
+    socket.broadcast.emit("changeConfig", arg);
+  });
 });
 
 httpServer.listen(5000);
